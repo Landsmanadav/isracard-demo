1155221e69497df5837dc18be9c9b78c694d14b0
Landsmanadav <Landsmanadav@gmail.com>
2025-07-11 15:05:41 +0300

    fix: remove old submodule reference and add frontend as regular folder

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#222222", // צבע רקע שלך
+          color: "#ffffff",
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Library System
+          </Typography>
+          <Button color="inherit" component={Link} to="/">
+            Main
+          </Button>
+          <Button color="inherit" component={Link} to="/books">
+            Books
+          </Button>
+          <Button color="inherit" component={Link} to="/members">
+            Members
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
