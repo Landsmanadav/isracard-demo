faa78e39f6e077f74ff25e443592c8662299d0b0
Landsmanadav <Landsmanadav@gmail.com>
2025-07-11 14:37:00 +0300

    frontend initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./styles/main.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Header from "./components/Header";
+import BooksPage from "./pages/BooksPage";
+import MembersPage from "./pages/MembersPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/members" element={<MembersPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
